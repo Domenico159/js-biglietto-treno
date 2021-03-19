f13267ad5153f209906569eb29e89e8c699a4255
Domenico159 <domenico.zummo@gmail.com>
2021-03-19 16:26:51 +0100

    Project Ultimate

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@
 function Area() {
     var km=parseInt(document.getElementById("km").value);
     var age=parseInt(document.getElementById("age").value);
-    var prezzoTotale=km*0.21.toFixed(1);
-    var prezzoOver= (prezzoTotale - (prezzoTotale*40)/100).toFixed(1);
-    var prezzoUnder=(prezzoTotale - (prezzoTotale*20)/100).toFixed(1);
+    var prezzoTotale=km*0.21.toFixed(2);
+    var prezzoOver= (prezzoTotale - (prezzoTotale*40)/100).toFixed(2);
+    var prezzoUnder=(prezzoTotale - (prezzoTotale*20)/100).toFixed(2);
     
    if(!isNaN(km)){
     if(!isNaN(age)){
