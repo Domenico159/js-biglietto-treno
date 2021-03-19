f903a67645eca80245e9ad6d7ea2c8ce8f93cafb
Domenico159 <domenico.zummo@gmail.com>
2021-03-19 16:26:03 +0100

    Project Ultimate

--- js/script.js
+++ js/script.js
@@ -5,21 +5,26 @@
 function Area() {
     var km=parseInt(document.getElementById("km").value);
     var age=parseInt(document.getElementById("age").value);
-    var prezzoTotale= km*0.21 ;
-    var prezzoOver= prezzoTotale - (prezzoTotale*40)/100;
-    var prezzoUnder=prezzoTotale - (prezzoTotale*20)/100;
+    var prezzoTotale=km*0.21.toFixed(1);
+    var prezzoOver= (prezzoTotale - (prezzoTotale*40)/100).toFixed(1);
+    var prezzoUnder=(prezzoTotale - (prezzoTotale*20)/100).toFixed(1);
     
-   if(!isNaN(age)){
-    if( (age >= 18) && (age <= 59) ) {
-        document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoTotale +'€';
+   if(!isNaN(km)){
+    if(!isNaN(age)){
+        if( (age >= 18) && (age <= 59) ) {
+            document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoTotale +'€';
+        }
+        else if(age <= 17){
+            document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoUnder +'€';
+        }
+        else if(age >= 60){
+            document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoOver +'€';
+        }
+       }else{
+        alert('ATTENZIONE! Carattere non corretto , per favore inserire un\'età in formato numerico, grazie.')
+       }
     }
-    else if(age <= 17){
-        document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoUnder +'€';
-    }
-    else if(age >= 60){
-        document.getElementById("prezzo").innerHTML = 'Prezzo: ' + prezzoOver +'€';
-    }
-   }else{
-    alert('Carattere non corretto, per favore inserire un numero')
+    else{
+    alert('ATTENZIONE! Carattere non corretto , per favore inserire i chilometri in formato numerico, grazie.')
 }
 }
